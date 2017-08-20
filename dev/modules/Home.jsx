ac1db09fd111da54525f1507094701fbd5965142
Joe.Kalb <kalb.joseph@gmail.com>
2017-08-19 23:12:18 -0700

    getting ready for redux

--- dev/modules/Home.jsx
+++ dev/modules/Home.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import Button from 'react-bootstrap/lib/Button';
 
 class Home extends React.Component {
 	
 	render() {
 		return (
 			<div id="container">
-				<a href="https://api.twitch.tv/kraken/oauth2/authorize?client_id=qu40xwrhqo12kl3g3isdqcma9rma8n&redirect_uri=http://localhost:3001&response_type=code&scope=user_read"><Button bsSize="large" block id="login">Login with Twitch</Button></a>
+				<a href="https://api.twitch.tv/kraken/oauth2/authorize?client_id=qu40xwrhqo12kl3g3isdqcma9rma8n&redirect_uri=http://localhost:3001&response_type=code&scope=user_read"><button>Login with Twitch</button></a>
 			</div>
 		);
 	}
